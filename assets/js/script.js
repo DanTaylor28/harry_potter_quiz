4341d84152c3af9dae30dd641b65b07a4872b7a4
Dan <dan.taylor1493@gmail.com>
2023-08-31 14:36:07 +0200

    Remove blur and popup when exit btn clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,14 @@
 const enterBtn = document.querySelector(".enter-btn");
 const homePopup = document.querySelector(".home-popup");
 const main = document.querySelector(".main");
+const exitBtn = document.querySelector(".exit-btn");
 
 enterBtn.onclick = () => {
   homePopup.classList.add("active");
   main.classList.add("active");
+};
+
+exitBtn.onclick = () => {
+  homePopup.classList.remove("active");
+  main.classList.remove("active");
 };
