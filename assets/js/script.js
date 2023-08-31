9314f020322ea688be32c924313015f2af5d03ce
Dan <dan.taylor1493@gmail.com>
2023-08-31 16:42:46 +0200

    Add final styling & layout to navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,16 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
 const enterBtn = document.querySelector(".enter-btn");
 const homePopup = document.querySelector(".home-popup");
 const main = document.querySelector(".main");
 const exitBtn = document.querySelector(".exit-btn");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
 
 enterBtn.onclick = () => {
   homePopup.classList.add("active");
