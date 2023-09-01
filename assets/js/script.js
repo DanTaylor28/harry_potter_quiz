0dde64c6e565f76c9cec6eba1268ce5e03d81951
Dan <dan.taylor1493@gmail.com>
2023-09-01 16:41:12 +0200

    Add comments to script.js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,25 +5,35 @@ const homePopup = document.querySelector(".home-popup");
 const main = document.querySelector(".main");
 const exitBtn = document.querySelector(".exit-btn");
 
+// EventListener to check for when hamburger icon is clicked & call
+// mobileMenu function
 hamburger.addEventListener("click", mobileMenu);
+// Function to toggle the navMenu when hamburger is clicked.
 function mobileMenu() {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
 }
 
+// EventListener listening out for a click on any of the navLinks and
+// calls closeMenu function when click occurs
 document.querySelectorAll(".nav-link").forEach((e) => {
   e.addEventListener("click", closeMenu);
 });
+// Closes the hamburger menu
 function closeMenu() {
   hamburger.classList.remove("active");
   navMenu.classList.remove("active");
 }
 
+// Assigns active class to homePopup & main classes to reveal the popup &
+// blur contents of the main class. 
 enterBtn.onclick = () => {
   homePopup.classList.add("active");
   main.classList.add("active");
 };
 
+// Removes active class from homePopup & main to hide popup & remove 
+// blur.
 exitBtn.onclick = () => {
   homePopup.classList.remove("active");
   main.classList.remove("active");
